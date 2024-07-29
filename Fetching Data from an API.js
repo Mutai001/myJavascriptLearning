f3c5a4c9e0bfb148bc4665e6fc67e8ea290f9b71
Mutai001 <omusekim@gmail.com>
2024-07-29 23:26:06 +0300

    Fetching Data from an API

---
+++ Fetching Data from an API.js
@@ -0,0 +1,5 @@
+// Fetch data from an API using the Fetch API
+fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching data:', error));
